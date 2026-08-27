7ffba557b52f493820976d7d7ba9d6f1f4bf96eb
aditiachari <amit.achari@dxc.com>
2026-08-27 15:52:05 +0530

    modified code

--- infra-aws/composition-modules/aws_ec2/versions.tf
+++ infra-aws/composition-modules/aws_ec2/versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  cloud {
+    organization = "amit-lab"
+
+    workspaces {
+      name = "ec2-dev"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
